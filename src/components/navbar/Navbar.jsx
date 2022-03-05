ea14af39ce8780d7d0e71a1ee2de97f39bb4a564
SRK-prog <srksivaram262@gmail.com>
2022-03-05 12:39:51 +0530

    changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./navbar.css";
-import Logo from "./netflix-logo.png";
-import userLogo from "./user.png";
+import Logo from "./icons/netflix-logo.png";
+import setIcon from "./icons/setIcon.png";
+import userLogo from "./icons/user.png";
 
 function Navbar() {
   return (
@@ -8,14 +9,15 @@ function Navbar() {
       <div className="logoWrap">
         <img className="navLogo" src={Logo} alt="Logo" />
       </div>
-      <div className="linkWrap">
-        <ul className="navlinks">
+      <div className="linkWrap ">
+        <ul className="navlinks display_none">
           <li className="navlink">Home</li>
           <li className="navlink">Movies</li>
           <li className="navlink">Tv shows</li>
         </ul>
       </div>
       <div className="userWrap">
+        <img className="userLogo" src={setIcon} alt="Logo" />
         <img className="userLogo" src={userLogo} alt="Logo" />
       </div>
     </nav>
