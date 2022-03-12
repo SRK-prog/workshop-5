7340e8d6c23c00eaf539e45d50480ecd34145558
SRK-prog <srksivaram262@gmail.com>
2022-03-12 12:56:16 +0530

    changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import "./navbar.css";
-import Logo from "../../icons/netflix-logo.png";
-import setIcon from "../../icons/setIcon.png";
-import userLogo from "../../icons/user.png";
 
 function Navbar() {
   const [navcolor, setNavcolor] = useState(false);
@@ -21,11 +18,11 @@ function Navbar() {
   return (
     <nav className={`navbarContainer ${navcolor && "nav-color"}`}>
       <div className="logoWrap">
-        <img className="navLogo" src={Logo} alt="Logo" />
+        <img className="navLogo" src="/icons/netflix-logo.png" alt="Logo" />
       </div>
       <div className="userWrap">
-        <img className="userLogo" src={setIcon} alt="Logo" />
-        <img className="userLogo" src={userLogo} alt="Logo" />
+        <img className="userLogo" src="/icons/setIcon.png" alt="Logo" />
+        <img className="userLogo" src="/icons/user.png" alt="Logo" />
       </div>
     </nav>
   );
