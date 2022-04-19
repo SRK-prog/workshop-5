b0ba57b57795d5874d079327dd04556a4efdb7df
Sivaramakrishnan_r <sivaramakrishnan@leewayhertz.com>
2022-04-19 10:38:30 +0530

    change

--- src/redux/reducers/postReducers.js
+++ src/redux/reducers/postReducers.js
@@ -3,7 +3,7 @@ import { FETCH_POST } from "../types";
 export default function fetchingPost(state = {}, action) {
   switch (action.type) {
     case FETCH_POST:
-      return action.payload;
+      return { ...action.payload };
     default:
       return state;
   }
